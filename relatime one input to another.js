3d799688ed1e7945d27e83cd78be2d8bde351f31
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-07-03 22:28:38 +0600

    Update relatime one input to another.js

--- relatime one input to another.js
+++ relatime one input to another.js
@@ -1,1 +1,19 @@
 https://jsfiddle.net/m9cudt3k/
+
+<form>
+<label>first name:</label><input id="firstName1"/>
+<label>last name:</label><input id="lastName1"/>
+</form>
+<br/>
+<form>
+<label>first name:</label><input id="firstName2"/>
+<label>last name:</label><input id="lastName2"/>
+</form>
+
+$("#firstName1").change(function (){
+$("#firstName2").val($(this).val());
+});
+
+$("#lastName1").keyup(function(){
+$("#lastName2").val($(this).val())
+});
