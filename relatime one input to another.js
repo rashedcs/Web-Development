c3155df0e0c1dcd3f63ee3ae84d4686808556ce4
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-07-05 21:59:09 +0600

    Update relatime one input to another.js

--- relatime one input to another.js
+++ relatime one input to another.js
@@ -2,6 +2,34 @@ https://jsfiddle.net/m9cudt3k/
 http://javascript-coder.com/javascript-form/javascript-calculator-script.phtml
 https://jsfiddle.net/flamedenise/fpovk96p/
 
+
+
+<script type='text/javascript' >
+function startCalc(){
+  interval = setInterval("calc()",1);
+}
+function calc(){
+  one = document.autoSumForm.firstBox.value;
+  two = document.autoSumForm.secondBox.value; 
+  document.autoSumForm.thirdBox.value = (one * 1) + (two * 1);
+}
+function stopCalc(){
+  clearInterval(interval);
+}
+</script>
+this is the html code
+
+<form name="autoSumForm">
+  
+<input class="right" type=text name="firstBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
++ <input class="right" type=text name="secondBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
+= <input class="right" type=text name="thirdBox">
+</form>
+
+
+
+
+<!--
 <form>
 <label>first name:</label><input id="firstName1"/>
 <label>last name:</label><input id="lastName1"/>
