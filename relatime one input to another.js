9502fe6851043d74cb6adc51716b4190caf027f1
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-07-06 22:47:32 +0600

    Update relatime one input to another.js

--- relatime one input to another.js
+++ relatime one input to another.js
@@ -1,91 +1,33 @@
 https://jsfiddle.net/m9cudt3k/
 http://javascript-coder.com/javascript-form/javascript-calculator-script.phtml
 https://jsfiddle.net/flamedenise/fpovk96p/
-https://www.youtube.com/watch?v=vkBiEuZSq9s
 http://javalearning-adventure.blogspot.com/2010/08/javascript-real-time-calculation.html
 http://ideone.com/BNpJrz
 
 
-<script type='text/javascript' >
-function startCalc(){
-  interval = setInterval("calc()",1);
+
+
+
+<!-- Reference : https://www.youtube.com/watch?v=vkBiEuZSq9s  -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script>
+function summation()
+{
+    var input1 = document.getElementById('input1').value;
+    var input2 = document.getElementById('input2').value;
+    var input3 = document.getElementById('input3').value;
+    var total = (input1*1) + (input2*1) +(input3*1);
+    document.getElementById('total').innerHTML = "Total = $"+total;
 }
-function calc(){
-  one = document.autoSumForm.firstBox.value;
-  two = document.autoSumForm.secondBox.value; 
-  document.autoSumForm.thirdBox.value = (one * 1) + (two * 1);
-}
-function stopCalc(){
-  clearInterval(interval);
-}
-</script>
-this is the html code
-
-<form name="autoSumForm">
-  
-<input class="right" type=text name="firstBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
-+ <input class="right" type=text name="secondBox" value="" onFocus="startCalc();" onBlur="stopCalc();"><br>
-= <input class="right" type=text name="thirdBox">
-</form>
-
-
-
-
-<!--
-<form>
-<label>first name:</label><input id="firstName1"/>
-<label>last name:</label><input id="lastName1"/>
-</form>
-<br/>
-<form>
-<label>first name:</label><input id="firstName2"/>
-<label>last name:</label><input id="lastName2"/>
-</form>
-
-$("#firstName1").change(function (){
-$("#firstName2").val($(this).val());
-});
-
-$("#lastName1").keyup(function(){
-$("#lastName2").val($(this).val())
-});
-
-
-2nd:
-
- <input id='first' type="text" class="form-control formBlock" name="bus_ticket"  placeholder="Bus Ticket..." required/><br />
-
-<input id='second' type="text" class="form-control formBlock" name="plane_ticket"  placeholder="Plane Ticket..." required/><br />
-
-<input id='third' type="text" class="form-control formBlock" name="hotel_expenses"  placeholder="Hotel Expenses..." required/><br />
-
- <input id='fourth' type="text" class="form-control formBlock" name="eating_expenses"  placeholder="Eating Expenses..." required/><br />
-<br /><br />
-Total: <span id="total_expenses1"></span>
-<br />
-Total: <input id='total_expenses2' type="text" class="form-control formBlock" name="funding"  placeholder="Total Expenses..."/>
-
-<br /><br />
-For:
-<a target="_blank" href="http://stackoverflow.com/questions/42730666/real-time-javascript-calculation-based-on-form-inputs">a stackoverflow question</a>
-
-$('input').keyup(function(){ // run anytime the value changes
-    var firstValue  = Number($('#first').val());   // get value of field
-    var secondValue = Number($('#second').val()); // convert it to a float
-    var thirdValue  = Number($('#third').val());
-    var fourthValue = Number($('#fourth').val());
-
-    $('#total_expenses1').html(firstValue + secondValue + thirdValue + fourthValue); // add them and output it
-    document.getElementById('total_expenses2').value = firstValue + secondValue + thirdValue + fourthValue;
-// add them and output it
-});
-
-/*
-$('input').keyup(function(){ // run anytime the value changes
-    var firstValue = parseFloat($('#first').val()); // get value of field
-    var secondValue = parseFloat($('#second').val()); // convert it to a float
-    var thirdValue = parseFloat($('#third').val());
-    var fourthValue = parseFloat($('#fourth').val());
-    document.getElementById('#total_expenses').value(firstValue + secondValue + thirdValue + fourthValue);
-// add them and output it
-});*/
+</script> 
+</head>
+<body>
+<p>Input 1 : <input id="input1" type="number"  onchange="summation()"></p>
+<p>Input 2 : <input id="input2" type="number"  onchange="summation()"></p>
+<p>Input 3 : <input id="input3" type="number"  onchange="summation()"></p>
+<p id="total"></p>
+</body>
+</html>
