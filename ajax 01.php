ffbab8059d310b963c3b930f40b93e04af0ebd64
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-07-19 02:34:20 +0600

    Update ajax 01.php

--- ajax 01.php
+++ ajax 01.php
@@ -5,72 +5,69 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
- 
-     <style>
-		.btn, .form-control
-		{
-			border-radius: 0;
-		}
-	</style>
-	</head>
+   <style>
+	.btn, .form-control
+	{
+		border-radius: 0;
+	}
+    </style>
+ </head>
  
 	<body>
-		<div class="container">
-			<div class="jumbotron">
-				<h1>School Record</h1>
-			</div>
- 
-			<div class="form-horizontal">
- 
-				<div class="form-group">
-					<label class="control-label col-md-2">Student Name </label>
-					<div class="col-md-8">
-						<input type="text" id="student_name" class="form-control">
-					</div>
-				</div>
- 
-				<div class="form-group">
-					<label class="control-label col-md-2">Student Subject </label>
-					<div class="col-md-3">
-						<select id="student_subject" class="form-control">
-							<option> Psychology </option>
-							<option> Bangla </option>
-						</select>
-					</div>
-				</div>
- 
-				<div class="form-group">
-				    <label class="control-label col-md-2">Student Fee</label>
-				    <div class="col-md-3">
-					   <input type="number" id="student_fee" class="form control">
-				   </div>
-				</div>
- 
- 
-				<div class="form-group">
-					<div class="col-md-8 col-md-offset-2">
-						<button class="btn btn-danger btn-block" onclick="myfunction();"> Add New Record </button>
-					</div>
+	   <div class="container">
+		<div class="jumbotron">
+			<h1>School Record</h1>
+		</div>
+		<div class="form-horizontal">
+
+			<div class="form-group">
+				<label class="control-label col-md-2">Student Name </label>
+				<div class="col-md-8">
+					<input type="text" id="student_name" class="form-control">
 				</div>
 			</div>
- 
-			<table class="table table-striped">
-					<thead> 
-						<tr class="success">
-							<th>S no </th>
-							<th> Student Name </th>
-							<th> Student Subject </th>
-							<th> Student Fee </th>
-							<th> Actions </th>
-						</tr>
-					</thead>
-					<tbody id="test">
-					</tbody>
-				</table>
+
+			<div class="form-group">
+				<label class="control-label col-md-2">Student Subject </label>
+				<div class="col-md-3">
+					<select id="student_subject" class="form-control">
+						<option> Psychology </option>
+						<option> Bangla </option>
+					</select>
+				</div>
 			</div>
-			
-			
-	   <script>
+
+			<div class="form-group">
+			    <label class="control-label col-md-2">Student Fee</label>
+			    <div class="col-md-3">
+				   <input type="number" id="student_fee" class="form control">
+			   </div>
+			</div>
+
+
+			<div class="form-group">
+				<div class="col-md-8 col-md-offset-2">
+					<button class="btn btn-danger btn-block" onclick="myfunction();"> Add New Record </button>
+				</div>
+			</div>
+		</div>
+
+		<table class="table table-striped">
+				<thead> 
+					<tr class="success">
+						<th>S no </th>
+						<th> Student Name </th>
+						<th> Student Subject </th>
+						<th> Student Fee </th>
+						<th> Actions </th>
+					</tr>
+				</thead>
+				<tbody id="test">
+				</tbody>
+		</table>
+	    </div>
+				
+	    <script>
 		function myfunction()
 		{
 			var student_name = document.getElementById('student_name').value;
@@ -89,8 +86,8 @@
 			 request.open("POST","data.php?name="+student_name+"&subject="+student_subject+"&fee="+student_fee,true);
 			 request.send();
 		}
-	   </script>
-</body>
+	     </script>
+    </body>
 </html>
 
 
